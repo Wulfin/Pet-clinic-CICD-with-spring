8b716e73f27bbf9c15c985bc4540a37301465e77
Wulfin <saifelislambettaoui1955@gmail.com>
2023-11-29 18:56:57 +0100

    initializing terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project     = "vocal-door-406417"
+  region      = "europe-west9"
+}
---
+++ terraform/resource.tf
@@ -0,0 +1,54 @@
+resource "google_compute_instance" "test" {
+  boot_disk {
+    auto_delete = true
+    device_name = "test"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20231115"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-medium"
+  name         = "test"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/vocal-door-406417/regions/europe-west9/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "1078491267775-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "europe-west9-a"
+}
